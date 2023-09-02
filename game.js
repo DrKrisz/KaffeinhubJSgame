8e984c5dfd8ddf77bbb236825226b9fc353c535d
Krisztian Bernath <70200514+DrKrisz@users.noreply.github.com>
2023-09-02 23:37:14 +0200

    selling caffe
    every 5 sec u selling a caffe and getting 5 dollar extrra

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
+// Initialize the money and caffe variables
 var money = 100;
 var caffe = 0;
+var timer = 5;
 
 // Function to update the money display
 function updateMoneyDisplay() {
@@ -9,6 +11,27 @@ function updateMoneyDisplay() {
 // Function to update the caffe display
 function updateCaffeDisplay() {
     document.getElementById("caffeDisplay").textContent = caffe;
+}
+
+// Function to update the timer display
+function updateTimerDisplay() {
+    document.getElementById("timerDisplay").textContent = timer + " seconds";
+}
+
+// Function to sell a kaffe and get money
+function sellKaffe() {
+    if (caffe >= 1) {
+        // Subtract 1 from caffe
+        caffe -= 1;
+        // Add 15 to money
+        money += 15;
+        // Update the displays
+        updateMoneyDisplay();
+        updateCaffeDisplay();
+        // Reset the timer
+        timer = 5;
+        updateTimerDisplay();
+    }
 }
 
 // Event listener for the button click
@@ -22,6 +45,9 @@ document.getElementById("spendButton").addEventListener("click", function() {
         // Update the displays
         updateMoneyDisplay();
         updateCaffeDisplay();
+        // Reset the timer
+        timer = 5;
+        updateTimerDisplay();
     } else {
         alert("Not enough money!");
     }
@@ -30,3 +56,13 @@ document.getElementById("spendButton").addEventListener("click", function() {
 // Initial displays
 updateMoneyDisplay();
 updateCaffeDisplay();
+updateTimerDisplay();
+
+// Set up a timer to sell kaffe every second and update the timer display
+setInterval(function() {
+    timer -= 1;
+    updateTimerDisplay();
+    if (timer <= 0) {
+        sellKaffe();
+    }
+}, 1000);
